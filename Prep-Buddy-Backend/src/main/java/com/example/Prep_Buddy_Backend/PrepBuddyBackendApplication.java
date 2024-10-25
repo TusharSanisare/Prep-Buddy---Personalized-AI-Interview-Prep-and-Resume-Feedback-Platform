1116b8e1a1115c804ca89117adbb1eb58554461c
TusharSanisare <tsanisare@gmail.com>
2024-10-26 01:14:45 +0530

    Setup...

---
+++ Prep-Buddy-Backend/src/main/java/com/example/Prep_Buddy_Backend/PrepBuddyBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.Prep_Buddy_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepBuddyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepBuddyBackendApplication.class, args);
+		System.out.println("Prep Buddy - Personalized AI Interview Prep and Resume Feedback Platform");
+		System.out.println("Started...");
+	}
+}
---
+++ Prep-Buddy-Backend/src/test/java/com/example/Prep_Buddy_Backend/PrepBuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prep_Buddy_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepBuddyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
